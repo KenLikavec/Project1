84ad925068af21ff224f3eb95e984c7f33c181c4
Ken Likavec <kdl17@zips.uakron.edu>
2016-07-08 21:02:25 -0400

    added in birth month problem

--- Project1/fortuneteller.cs
+++ Project1/fortuneteller.cs
@@ -33,18 +33,96 @@ namespace Project1
             Console.WriteLine();
 
             Console.WriteLine("What ROYGBIV color is your favorite? (Any questions concerning \"ROYGBIV\" type out the word \"help\" then press enter");
-            Console.ReadLine();
-
-            if (Console.ReadLine() == Help)
+            string favColor = Console.ReadLine();
+            while(favColor == "help")
             {
-                Console.WriteLine("\"ROYGBIV\" stands for Red, Orange, Yellow, Green, Indigo and violet");
+                Console.WriteLine("\"ROYGBIV\" stands for red, orange, yellow, green, blue, indio and violet");
+                Console.WriteLine("So which of these options are your favorite?");
+                favColor = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("How many siblings do you have in your family?");
+            int siblings = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Now for your fortune results...");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if(age %2 != 0)
+            {
+                Console.WriteLine("You will retire in 93 years! Oh No!");
             }
             else
             {
-                string favColor = Console.ReadLine();
+                Console.WriteLine("You will retire in half a year! Because you'll win the lotto!");
             }
 
+            Console.WriteLine();
 
+            if(siblings == 0)
+            {
+                Console.WriteLine("You will soon live in the Rain Forest!");
+            }
+            if(siblings == 1)
+            {
+                Console.WriteLine("You will soon live on Mars!");
+            }
+            if(siblings == 2)
+            {
+                Console.WriteLine("You will soon live in in the Sahara Desert!");
+            }
+            if(siblings == 3)
+            {
+                Console.WriteLine("You will soon live in Atlantis");
+            }
+            if(siblings > 3)
+            {
+                Console.WriteLine("You will soon live in a haunted house!");
+            }
+
+            Console.WriteLine();
+
+            if(favColor == "red")
+            {
+                Console.WriteLine("You will now get from place to place via hovercraft");
+            }
+            if(favColor == "orange")
+            {
+                Console.WriteLine("You will now get from place to place via spaceship");
+            }
+            if(favColor == "yellow")
+            {
+                Console.WriteLine("You will now get from place to place via your feet");
+            }
+            if(favColor == "green")
+            {
+                Console.WriteLine("You will now get from place to place via bigfoot");
+            }
+            if(favColor == "blue")
+            {
+                Console.WriteLine("You will now get from place to place via teleportation");
+            }
+            if(favColor == "indigo")
+            {
+                Console.WriteLine("You will now get from place to place via transformer");
+            } 
+            if(favColor == "violet")
+            {
+                Console.WriteLine("You will now get from place to place via delorean");
+            }
+
+            Console.WriteLine();
+
+            string[] birthMonthArray = { birthMonth };
+            string[] firstNameArray = { firstName };
+            string[] lastNameArray = { lastName };
+
+            if (birthMonthArray[0] == firstNameArray[0] || lastNameArray[0])
 
             Console.ReadKey();
 
