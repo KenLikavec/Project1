65a3d578c4e9da880339ad4be09d582d3e85866c
Ken Likavec <kdl17@zips.uakron.edu>
2016-07-10 20:46:18 -0400

    As far as I could get

--- Project1/fortuneteller.cs
+++ Project1/fortuneteller.cs
@@ -57,10 +57,12 @@ namespace Project1
             if (age % 2 != 0)
             {
                 Console.WriteLine("93 years");
+                string ageAnswer = "93 years";
             }
             else
             {
-                Console.WriteLine("6 months!");
+                Console.WriteLine("6 months");
+                string ageAnswer = "6 months";
             }
 
             Console.WriteLine();
@@ -68,26 +70,32 @@ namespace Project1
             if (siblings == 0)
             {
                 Console.WriteLine("Rain Forest");
+                string siblingsAnswer = "Rainforest";
             }
             else if (siblings == 1)
             {
                 Console.WriteLine("Mars");
+                string siblingsAnswer = "Mars";
             }
             else if (siblings == 2)
             {
                 Console.WriteLine("Sahara Desert");
+                string siblingsAnswer = "Sahara Desert";
             }
             else if (siblings == 3)
             {
                 Console.WriteLine("Atlantis");
+                string siblingsAnswer = "Atlantis";
             }
             else if (siblings > 3)
             {
-                Console.WriteLine("th moon");
+                Console.WriteLine("the Moon");
+                string siblingsAnswer = "the Moon";
             }
             else
             {
-                Console.WriteLine("N/A");
+                Console.WriteLine("Rome");
+                string siblingsAnswer = "Rome";
             }
 
             Console.WriteLine();
@@ -95,40 +103,71 @@ namespace Project1
             if (favColor == "red")
             {
                 Console.WriteLine("hovercraft");
+                string favColorAnswer = "hovercraft";
             }
             else if (favColor == "orange")
             {
                 Console.WriteLine("spaceship");
+                string favColorAnswer = "Spaceship";
             }
             else if (favColor == "yellow")
             {
-                Console.WriteLine("foot");
+                Console.WriteLine("BMW");
+                string favColorAnswer = "BMW";
             }
             else if (favColor == "green")
             {
-                Console.WriteLine("bigfoot");
+                Console.WriteLine("Mercedes");
+                string favColorAnswer = "Mercedes";
             }
             else if (favColor == "blue")
             {
                 Console.WriteLine("teleportation");
+                string favColorAnswer = "teleportation";
             }
             else if (favColor == "indigo")
             {
                 Console.WriteLine("transformer");
+                string favColorAnswer = "Transformer";
             }
             else if (favColor == "violet")
             {
                 Console.WriteLine("delorean");
+                string favColorAnswer = "Delorean";
             }
             else
             {
-                Console.WriteLine("N/A");
+                Console.WriteLine("Garbage truck");
+                string favColorAnswer = "Garbage Truck";
             }
             
 
             Console.WriteLine();
 
+            int index1 = firstName.IndexOf(birthMonth[0]);
+            int index2 = lastName.IndexOf(birthMonth[0]);
+            int index3 = firstName.IndexOf(birthMonth[1]);
+            int index4 = lastName.IndexOf(birthMonth[1]);
+            int index5 = firstName.IndexOf(birthMonth[2]);
+            int index6 = lastName.IndexOf(birthMonth[2]);
+            if (index1 != -1 || index2 != -1)
+            {
+                Console.WriteLine("$0");
+            }
+            else if (index3 != -1 || index4 != -1)
+            {
+                Console.WriteLine("$1,000,000");
+            }
+            else if (index5 != -1 || index6 != -1)
+            {
+                Console.WriteLine("$100");
+            }
+            else
+            {
+                Console.WriteLine("0.50");
+            }
 
+            //Console.WriteLine(firstName + "" + lastName + "" + "will retire in " + favColorAnswer)
                 
 
 
